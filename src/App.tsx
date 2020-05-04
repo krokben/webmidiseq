a33478595a2f7feeb7cc7546579cbda70a14f09a
Tommy Svensson <tommy@diesdas.digital>
2020-05-04 22:17:48 +0200

    Loop through the 8 bars

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Steps from "./components/Steps";
 
 export default () => {
+  const [bpm, setBpm] = useState(120);
+  const [meter, setMeter] = useState([4, 4]);
   const [steps, setSteps] = useState([
     { position: 1, note: "c4" },
     { position: 2, note: "c4" },
@@ -13,10 +15,53 @@ export default () => {
     { position: 7, note: "c4" },
     { position: 8, note: "c4" },
   ]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [activePosition, setActivePosition] = useState(1);
+  const oneBar = (60 / (bpm / meter[0])) * 1000;
+  const [nextIncrease, setNextIncrease] = useState(oneBar);
+
+  useInterval(
+    () => {
+      setActivePosition(
+        activePosition === steps.length ? 1 : activePosition + 1
+      );
+    },
+    isPlaying ? oneBar : null
+  );
+
+  const handlePlayButtonClick = () => {
+    setIsPlaying(!isPlaying);
+    setActivePosition(1);
+  };
 
   return (
     <main>
-      <Steps steps={steps} />
+      <Steps steps={steps} activePosition={activePosition} />
+      <button onClick={handlePlayButtonClick}>Play/Reset</button>
     </main>
   );
 };
+
+function useInterval(callback, delay) {
+  /*
+  /* Thanks to Dan Abramov for this.
+  /* https://overreacted.io/making-setinterval-declarative-with-react-hooks
+  */
+  const savedCallback = useRef();
+
+  // Remember the latest function.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 
-export default ({ steps }) => (
+export default ({ steps, activePosition }) => (
   <ul>
-    {steps.map(({ position, note }) => (
-      <li key={`step-${position}`}>{note}</li>
+    {steps.map(({ position, note }, index) => (
+      <li
+        key={`step-${position}`}
+        style={index + 1 == activePosition ? { color: "red" } : {}}
+      >
+        {note}
+      </li>
     ))}
   </ul>
 );
