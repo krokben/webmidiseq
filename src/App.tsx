584412d0b9d02217af042956b0d02b57ed36eaa0
Tommy Svensson <tommy@diesdas.digital>
2020-05-04 23:26:22 +0200

    basic midi playback

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import * as React from "react";
 import { useState, useEffect, useRef } from "react";
+import WebMidi from "webmidi";
 import Steps from "./components/Steps";
 
+const MIDI_OUTPUT_DEVICE_NAME = "Steinberg UR22mkII";
+
 export default () => {
-  const [bpm, setBpm] = useState(120);
+  const [io, setIO] = useState({ output: {} });
+  const [bpm, setBPM] = useState(120);
   const [meter, setMeter] = useState([4, 4]);
   const [steps, setSteps] = useState([
-    { position: 1, note: "c4" },
-    { position: 2, note: "c4" },
-    { position: 3, note: "c4" },
-    { position: 4, note: "c4" },
-    { position: 5, note: "c4" },
-    { position: 6, note: "c4" },
-    { position: 7, note: "c4" },
-    { position: 8, note: "c4" },
+    { position: 1, note: "C4" },
+    { position: 2, note: "C4" },
+    { position: 3, note: "C4" },
+    { position: 4, note: "C4" },
+    { position: 5, note: "C4" },
+    { position: 6, note: "C4" },
+    { position: 7, note: "C4" },
+    { position: 8, note: "C4" },
   ]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [activePosition, setActivePosition] = useState(1);
   const oneBar = (60 / (bpm / meter[0])) * 1000;
   const [nextIncrease, setNextIncrease] = useState(oneBar);
+
+  useEffect(() => {
+    WebMidi.enable((err) => {
+      if (err) {
+        console.error("WebMidi could not be enabled.", err);
+        return;
+      }
+      setIO({ output: WebMidi.getOutputByName(MIDI_OUTPUT_DEVICE_NAME) });
+    });
+  }, []);
 
   useInterval(
     () => {
@@ -33,6 +47,14 @@ export default () => {
     setIsPlaying(!isPlaying);
     setActivePosition(1);
   };
+
+  if (isPlaying && io.output.playNote) {
+    io.output.playNote(
+      steps.find((step) => step.position === activePosition).note,
+      "all",
+      { duration: oneBar - 1 }
+    );
+  }
 
   return (
     <main>
